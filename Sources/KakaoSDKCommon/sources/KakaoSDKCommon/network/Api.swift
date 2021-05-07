fb7b5fa7a54233d02a14dc9cdb4f23de34fcce41
box-jeon <utrpanic@gmail.com>
2021-05-08 00:43:50 +0900

    KakaoSDK compile error 수정.

--- Sources/KakaoSDKCommon/sources/KakaoSDKCommon/network/Api.swift
+++ Sources/KakaoSDKCommon/sources/KakaoSDKCommon/network/Api.swift
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-import Foundation
+import UIKit
 import Alamofire
 
 public let API = Api.shared
